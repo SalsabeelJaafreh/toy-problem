0e6c9edb015d878b32a0302313c6897546383b4c
Salsabeel <s.jaafreh@yahoo.com>
2018-07-29 22:10:08 +0300

    solved

--- filterPalindromes/filterPalindromes.js
+++ filterPalindromes/filterPalindromes.js
@@ -5,11 +5,30 @@
 
 
 
-
-
-
-
 function filterPalindromes (words) {
   // Write your code here, and
   // return your final answer.
+  var array = [] ;
+  var ispalin = false ;
+      for ( var i = 0 ; i < words.length ; i++ ){
+        var temp =words[i].replace(" ")
+
+      for ( var x = 0 ; x < temp.length ; x++ ){
+      for(var k=temp.length-1;k>=0;k--){
+         if (temp[x].toLowerCase() === temp[k].toLowerCase())
+         {
+         ispalin = true;
+         }
+
+        else {
+        ispalin = false;
+            }
+         }
+     } 
+    if(ispalin === true){
+      array.push(words[i])
+    }
+  }
+  return array
+  
 }
