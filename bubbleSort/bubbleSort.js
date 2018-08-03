1b6a81bc03be2a9d14a837e4cae0f33d4ca4b9c6
Salsabeel <s.jaafreh@yahoo.com>
2018-08-03 12:14:41 +0300

    sloved easy one !

--- bubbleSort/bubbleSort.js
+++ bubbleSort/bubbleSort.js
@@ -7,6 +7,17 @@
 //Implement a function that takes an array and sorts it using this technique.
 //NOTE: DO NOT use JavaScript’s built-in sorting function (Array.prototype.sort).
 var bubbleSort = function(array) {
-	
-};
+  var temp;
+for(var i=0;i<array.length;i++){
+  for (var j=0;j<array.length;j++){
+    if(array[j]>array[j+1]){
+      temp=array[j]
+      array[j]=array[j+1]
+      array[j+1]=temp
+    }
+  }
+}
+return array;
+}
+
 
