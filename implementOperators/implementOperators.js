f39939a2373ac459fabace24e154d6a3c793ce36
Salsabeel <s.jaafreh@yahoo.com>
2018-08-16 15:43:36 +0300

    solve it

--- implementOperators/implementOperators.js
+++ implementOperators/implementOperators.js
@@ -6,23 +6,35 @@ EXAMPLES:
 multiply(5, 2) === 10
 divide(5, 2) === 2
 modulo(5, 2) === 1  */
+
 var multiply = function(x, y) {
 
-
-
-}
+    if(x == 0|| y == 0){
+        return 0;
+    }
+    else if(y > 0) {
+        return x + multiply(x, y - 1);
+    }
+     return - multiply(x, -y);
+};
 
 
 var divide = function(x, y) {
-
-
-
-
+if(x===0&&y==0){
+  return "NaN"
 }
+   var count =0;
+  for(var i=y;i>0;i--){
+     x=x-y
+    count++
+}
+   return count;
+  
+};
 
 var modulo = function(x, y) {
-
-
-
-
+  for(var i=x;i>=y;i--){
+       x=x-y
 }
+    return x;
+};
