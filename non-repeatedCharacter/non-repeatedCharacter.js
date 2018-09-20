ff795403bf85e3f39d503503422061b1465ce587
Salsabeel Jaafreh <s.jaafreh@yahoo.com>
2018-09-20 15:40:40 +0300

    Update non-repeatedCharacter.js

--- non-repeatedCharacter/non-repeatedCharacter.js
+++ non-repeatedCharacter/non-repeatedCharacter.js
@@ -21,5 +21,42 @@ string:
 */
 
 function firstNonRepeatedCharacter (string) {
-   // write your code here
+  // Write your code here, and
+  var count=0;
+  for(var i=0;i<string.length;i++){
+    for(var j=1;j<string.length;j++){
+      if(string[i]!==string[j]){
+        count=0
+        
+      }
+      else{count++}
+      
+    }
+   if(count===0){
+    return string[i];
+   }
+  }
+  return "sorry"
+  
+  
 }
+  
+  
+  
+  
+
+  
+//   var ar=string.split('')
+//   for(var i=0;i<ar.length;i++){
+//   for(var j=1;j<ar.length;j++) {
+//   if(ar[i]!==ar[j]){
+//   }
+  
+// }
+// return ar[i]
+// }
+// return "sorry"
+// }
+
+  
+  
