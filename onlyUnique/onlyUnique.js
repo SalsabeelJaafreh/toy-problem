ab1700cc83cc33cf95fb68db9174c458a9935178
Salsabeel Jaafreh <s.jaafreh@yahoo.com>
2018-09-20 15:12:29 +0300

    Update onlyUnique.js

--- onlyUnique/onlyUnique.js
+++ onlyUnique/onlyUnique.js
@@ -19,5 +19,36 @@ str:                  |
 */
 
 function onlyUnique (str) {
-   // your code is here
-}	
+  // Write your code here, and
+  // return your final answer.
+  var count=0;
+  var str1=''
+  for(var i=0;i<str.length;i++){
+    count=0 
+  for(var j=0;j<str.length;j++){
+    if(str[i]===str[j]){
+        count++
+    }
+  }
+  if(count===1){
+    str1+=str[i];
+  }
+  }
+  return str1;
+  // var str1 = '';
+  // var obj = {};
+  // for (var i = 0; i<str.length ; i++){
+  //     obj[str[i]]=0
+  // }
+  // for(var j =0 ; j<str.length ;j++){
+  //   if(obj.hasOwnProperty(str[j])){
+  //     obj[str[j]]+=1
+  //   }
+  // }
+  // for(var key in obj){
+  //   if(obj[key] === 1){
+  //     str1+=key
+  //   }
+  // }
+  // return str1
+}
